9f8804af515bf3e0f5b8a1d435c1add1dbb53327
Chris Mather <mather.chris@gmail.com>
2013-04-16 17:08:46 -0700

    final

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/************************ Both ***********************************************/
+/************************ Helpers ********************************************/
 typeOf = function (value) {
   if (typeof value.typeName === 'function')
     return value.typeName();
@@ -9,72 +9,81 @@ typeOf = function (value) {
 bufferToString = function (buffer) {
   return String.fromCharCode.apply(null, buffer);
 };
+/*****************************************************************************/
 
-function Address (value) {
-  this.value = value;
+function Address (city, state) {
+  this.city = city;
+  this.state = state;
 }
 
-Address.prototype.get = function () {
-  return this.value;
+Address.prototype = {
+  constructor: Address,
+
+  toString: function () {
+    return this.city + ', ' + this.state;
+  },
+
+  // Return a copy of this instance
+  clone: function () {
+    return new Address(this.city, this.state);
+  },
+
+  // Compare this instance to another instance
+  equals: function (other) {
+    if (!(other instanceof Address))
+      return false;
+
+    return EJSON.stringify(this) == EJSON.stringify(other);
+  },
+
+  // Return the name of this type which should be the same as the one
+  // padded to EJSON.addType
+  typeName: function () {
+    return "Address";
+  },
+
+  // Serialize the instance into a JSON-compatible value. It could
+  // be an object, string, or whatever would naturally serialize
+  // to JSON
+  toJSONValue: function () {
+    return {
+      city: this.city,
+      state: this.state
+    };
+  }
 };
+
+// Tell EJSON about our new custom type
+EJSON.addType("Address", function fromJSONValue(value) {
+  // the parameter - value - will look like whatever we
+  // returned from toJSONValue from above.
+  console.log(value);
+  return new Address(value.city, value.state);
+});
 
 person = {
   name: "Chris Mather",
 
-  // EJSON can handle dates out of the box
   createdAt: new Date(),
 
-  // EJSON can handle Uint8Arrays out of the box.
-  // This value is just a utf encoded string that
-  // says "hello". You can try it out in the browser
-  // by typing > bufferToString(person.file)
   file: new Uint8Array([104, 101, 108, 108, 111]),
 
-  // EJSON doesn't know how to serialize and deserialize this
-  // value yet
-  address: new Address("San Francisco")
+  address: new Address("San Francisco", "CA")
 };
-/*****************************************************************************/
 
 People = new Meteor.Collection("people");
 
 /************************ Client *********************************************/
-
 if (Meteor.isClient) {
   Meteor.subscribe("people");
-
-  uploadPerson = function () {
-    // person automatically serialized into EJSON compatible JSON
-    // before being sent over the wire
-    Meteor.call("uploadPerson", person);
-  }
 }
-
 /*****************************************************************************/
 
-
 /************************ Server *********************************************/
-
 if (Meteor.isServer) {
   Meteor.publish("people", function () {
-    // person is automatically serialized into EJSON before being sent
-    // across the wire where it will be deserialized the the correct
-    // date and binary property types
     this.added("people", Meteor.uuid(), person);
-
-    // tell the client we're done sending messages
     this.ready();
   });
-
-  Meteor.methods({
-    // person is automatically deserialized from JSON into an EJSON type
-    // with dates and binary deserialized into the correct type
-    // automatically
-    uploadPerson: function (person) {
-      console.log("typeOf createdAt: ", typeOf(person.createdAt));
-      console.log("typeOf file: ", typeOf(person.file));
-    }
-  });
 }
-
 /*****************************************************************************/
